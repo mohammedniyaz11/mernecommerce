3e6e6617270b56609a627dbe518d993686a6ac67
mohammed niyaz <mohemmedniyaz11@gmail.com>
2022-07-31 21:27:34 +0530

    committed

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header >
+    <a href="/">amazona</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
